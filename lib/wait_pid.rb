67efdb37c125af4f851ce675be265e9bd3e981f3
rdp <rogerpack2005@gmail.com>
2009-12-18 09:51:45 -0700

    first passing test for doze

---
+++ lib/wait_pid.rb
@@ -0,0 +1,22 @@
+class WaitPid
+
+  def self.wait_pid(pid, test = false)
+    # initial test
+    count = 0
+    begin
+      loop { Process.kill( 0, pid); count += 1; sleep 0.1}
+    rescue Errno::ESRCH
+      if count == 0
+        if test
+          return "non existing"
+        else
+          puts "warning: pid not found #{pid}" if $VERBOSE
+        end
+      else
+        # normal
+      end
+    end
+
+  end
+
+end
